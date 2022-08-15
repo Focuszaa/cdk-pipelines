dc72a39b1699d230c9ce201272998674d8d94396
Focuszaa <Focuszaaphoto@gmail.com>
2022-08-15 23:10:22 +0700

    test runing!

--- lib/cdk-pipelines-stack.ts
+++ lib/cdk-pipelines-stack.ts
@@ -1,17 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './cdk-pipeline-app-stage';
 
 export class CdkPipelinesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
+      pipelineName: 'CdkPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('Focuszaa/cdk-pipelines', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "892456250180", region: "eu-east-1" }
+    }));
   }
 }
