0185f1dc7a54968283cc95bf40a94db1cc326292
npheng <nirut.phengjaiwong@amexgbt.com>
2022-08-16 17:50:25 +0700

    feat (cdk): 🏗  deployment by separating ENV

--- bin/cdk-pipelines.ts
+++ bin/cdk-pipelines.ts
@@ -1,13 +1,54 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkPipelinesStack } from '../lib/cdk-pipelines-stack';
+import { CdkPipelinesStack, CdkPipelineStackRemoteProps } from '../lib/cdk-pipelines-stack';
+import constants from '../lib/constants';
 
 const app = new cdk.App();
-new CdkPipelinesStack(app, 'CdkPipelinesStack', {
-  env: {
-    account: '892456250180',
-    region: 'us-east-1',
-  }
+
+const { AWS: {  PROFILE }, API } = constants;
+
+console.log('Starting.....')
+const revision = require('child_process').execSync('git rev-parse HEAD').toString().trim();
+
+// prepare for using Dynamodb
+// const DbTableStack = new TableStack(app, 'Db1Table', {
+//   env: { account: PROFILE.ACCOUNT, region: PROFILE.REGION },
+//   tableName: DYNAMODB_TABLES.DB1
+// });
+
+const defaultProps = new CdkPipelineStackRemoteProps({
+  stageName: '',
+  route53ZoneName: API.HOSTNAME,
+  env: { account: PROFILE.ACCOUNT, region: PROFILE.REGION },
+  description: `Git Revision: ${revision}`,
 });
+const canaryProps = new CdkPipelineStackRemoteProps({
+  ...defaultProps,
+  stageName: 'canary',
+});
+const stagingProps = new CdkPipelineStackRemoteProps({
+  ...defaultProps,
+  stageName: 'staging',
+});
+const productionProps = new CdkPipelineStackRemoteProps({
+  ...defaultProps,
+  stageName: 'production',
+});
+
+const canaryCdkPipelineStack      = new CdkPipelinesStack(app, 'canaryCdkPipelineStack', canaryProps);
+const stagingCdkPipelineStack     = new CdkPipelinesStack(app, 'stagingCdkPipelineStack', stagingProps);
+const productionCdkPipelineStack  = new CdkPipelinesStack(app, 'productionCdkPipelineStack', productionProps);
+
+cdk.Tags.of(canaryCdkPipelineStack).add('environment', 'canary');
+cdk.Tags.of(stagingCdkPipelineStack).add('environment', 'staging');
+cdk.Tags.of(productionCdkPipelineStack).add('environment', 'production');
+
+
+// new CdkPipelinesStack(app, 'CdkPipelinesStack', {
+//   env: {
+//     account: '892456250180',
+//     region: 'us-east-1',
+//   }
+// });
 
 app.synth();
